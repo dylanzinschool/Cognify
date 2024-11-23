318d05b843b2756aa131642d17fd51877b93b621
dylanzinschool <dylanwkzin@gmail.com>
2024-11-23 21:12:45 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,27 +12,28 @@ self.addEventListener('install', event => {
   })());
 });
 
-//self.addEventListener('fetch', event => {
-//  if(event.request.url.indexOf('firestore.googleapis.com') === -1) {
- // event.respondWith((async () => {
- // const cache = await caches.open(CACHE_NAME);
-//
+self.addEventListener('fetch', event => {
+  if(event.request.url.indexOf('firestore.googleapis.com') === -1) {
+  event.respondWith((async () => {
+  const cache = await caches.open(CACHE_NAME);
+
   // Get the resource from the cache.
-  //  const cachedResponse = await cache.match(event.request);
-   // if (cachedResponse) {
-  //    return cachedResponse;
-  //    } else {
-   //     try {
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+      } else {
+        try {
           // If the resource was not in the cache, try the network.
-   //     const fetchResponse = await fetch(event.request);
+        const fetchResponse = await fetch(event.request);
 
           // Save the resource in the cache and return it.
-   //       cache.put(event.request, fetchResponse.clone());
-   //       return fetchResponse;
-   //    } catch (e) {
-   //       // The network failed.
-  //     }
-  // }
- ////})());
-//}
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+       } catch (e) {
+          // The network failed.
+       }
+   }
+ })());
+}
+});
 //});
